7ee1f7dd5e63b21737d2547e83e49119cd4d8bd3
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-01-13 19:51:58 +0300

    broken but my, broken views and html

--- webstatistics/appstat/models.py
+++ webstatistics/appstat/models.py
@@ -8,19 +8,31 @@ class Poll(models.Model):
     def __str__(self):
         return self.name
 
+    
+    class Meta:
+        verbose_name_plural = 'Polls'
+
 
 class Question(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.DO_NOTHING)
+    poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question_text
 
+  
+    class Meta:
+        verbose_name_plural = 'Questions'
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+
+    class Meta:
+        verbose_name_plural = 'Choices'
--- webstatistics/appstat/views.py
+++ webstatistics/appstat/views.py
@@ -1,28 +1,70 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice, Poll
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-question_text')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'appstat/index.html', context)
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-question_text')[:5]
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    return render(request, 'appstat/index.html', context)
 
 # def poll(request, poll_id):
 #    return HttpResponse(f"You're looking at poll {poll_id}")
+class IndexView(generic.ListView):
+    template_name = 'appstat/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.order_by('-date_pub')
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'appstat/detail.html', {'question': question})
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'appstat/questions.html', {'question': question})
+#class QuestionView(generic.ListView):
+#    model = Question
+#    template_name = 'appstat/questions.html'
+#    context_object_name = 'poll_questions'
+#
+#    def get_queryset(self):
+#        return Question.objects.filter(Question, pk='question_id')
+def questionview(request, poll_id):
+    poll_questions = Question.objects.filter(pk=poll_id)
+    context = {
+    'poll_questions': poll_questions,
+    }
+#    return render(request, 'appstat/index.html', context)
+#   question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'appstat/questions.html', context)
+    
 
 
-def results(request, question_id):
-    return HttpResponse(f"Results of question {question_id}")
 
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'appstat/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'appstat/results.html'
 
+    
 def vote(request, question_id):
-    return HttpResponse(f"Your vote on question is {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST['choice']) 
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'appstat/detail.html', {
+            'question': question,
+            'error_message':'You did not select a choice',
+        })
+    else: 
+        selected_choice.votes +=1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polling:results'), args=(question.id,))
+
 
