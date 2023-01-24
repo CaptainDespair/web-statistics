b93704002da2eb511b97d99a88c5ccb574cfd7d4
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-01-24 21:30:54 +0300

    +EXCEPTIONS(return q.html)

--- webstatistics/appstat/urls.py
+++ webstatistics/appstat/urls.py
@@ -6,5 +6,5 @@ app_name = 'polling'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:poll_id>/', views.questionview, name='questions'),
-    path('<int:poll_id>/results/', views.results, name='results'),
+    path('<int:poll_id>/results/', views.results, name='results'), 
 ]
--- webstatistics/appstat/views.py
+++ webstatistics/appstat/views.py
@@ -1,20 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.shortcuts import render
 from django.views import generic
-
 from .models import Question, Choice, Poll
 
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-question_text')[:5]
-#    context = {
-#        'latest_question_list': latest_question_list,
-#    }
-#    return render(request, 'appstat/index.html', context)
-
-# def poll(request, poll_id):
-#    return HttpResponse(f"You're looking at poll {poll_id}")
 class IndexView(generic.ListView):
     template_name = 'appstat/index.html'
     context_object_name = 'latest_poll_list'
@@ -23,16 +11,6 @@ class IndexView(generic.ListView):
         return Poll.objects.order_by('date_pub')
 
 
-#def detail(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'appstat/questions.html', {'question': question})
-#class QuestionView(generic.ListView):
-#    model = Question
-#    template_name = 'appstat/questions.html'
-#    context_object_name = 'poll_questions'
-#
-#    def get_queryset(self):
-#        return Question.objects.filter(Question, pk='question_id')
 def questionview(request, poll_id):
     poll_questions = Question.objects.filter(poll_id=poll_id) 
     polling_name = Poll.objects.get(pk=poll_id)
@@ -46,33 +24,21 @@ def questionview(request, poll_id):
 def results(request, poll_id):
     poll_questions = Question.objects.filter(poll_id=poll_id)
     polling_name = Poll.objects.get(pk=poll_id)
-    for question in poll_questions:
-        print(question.poll)
-        print(question)
-        print(question.id) 
-        selected_choice = question.choices.get(pk=request.POST[question.question_text]) 
-        selected_choice.votes +=1
-        selected_choice.save()
-    context = {
-        'poll_questions' : poll_questions,
-        'polling_name': polling_name,    
-    }
-    return render(request, 'appstat/results.html', context)
-
-#   try:
-#         selected_choice = question.choices.get(pk=request.POST['choice']) 
-#     except (KeyError, Choice.DoesNotExist):
-#         return render(request, 'appstat/questions.html', {
-#             'question': question,
-#             'error_message':'You did not select a choice',
-#         })
-#     else: 
-#         selected_choice.votes +=1
-#         selected_choice.save()
-#     context = {
-#         'poll_questions' : poll_questions,
-#         'question': question,
-#         'polling_name': polling_name,
-#     }
-#     return render(request, 'appstat/results.html', context)
-
+    try: 
+        for question in poll_questions:
+            selected_choice = question.choices.get(pk=request.POST[question.question_text]) 
+            selected_choice.votes +=1
+            selected_choice.save()
+    except(Choice.DoesNotExist, KeyError):
+        context = {
+            'poll_questions': poll_questions,
+            'polling_name' : polling_name,
+            'error_message':'Проголосуйте во всех вопросах!',
+        }
+        return render(request, 'appstat/questions.html', context)
+    else:       
+        context = {
+            'poll_questions' : poll_questions,
+            'polling_name': polling_name,    
+        }
+        return render(request, 'appstat/results.html', context)
