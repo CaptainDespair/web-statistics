71f966d89100613be4b3c1680e345fea93165dca
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-02-08 01:56:42 +0300

    results:your voice is... CSS! (+logic)

--- webstatistics/appstat/views.py
+++ webstatistics/appstat/views.py
@@ -27,19 +27,13 @@ def questionview(request, poll_id):
 def results(request, poll_id):
     poll_questions = Question.objects.filter(poll_id=poll_id)
     polling_name = Poll.objects.get(pk=poll_id)
-    try: 
+    try:
         selected_choice_list = []
-        max_questions = []
-        for question in poll_questions:
-               
+        for question in poll_questions:  
             selected_choice = question.choices.get(pk=request.POST[question.question_text]) 
             selected_choice.votes +=1
             selected_choice.save()
-            selected_choice_list.append(selected_choice.votes)
-            max_choice = max(selected_choice_list)
-            print(max_choice)
-            selected_choice_list = []
-            max_questions.append(max_choice)      
+            selected_choice_list.append(selected_choice)
     except(Choice.DoesNotExist, KeyError):
         context = {
             'poll_questions': poll_questions,
@@ -51,6 +45,6 @@ def results(request, poll_id):
         context = {
             'poll_questions' : poll_questions,
             'polling_name': polling_name,
-            'max_questions': max_questions,    
+            'selected_choice_list' : selected_choice_list,    
         }
         return render(request, 'appstat/results.html', context)
