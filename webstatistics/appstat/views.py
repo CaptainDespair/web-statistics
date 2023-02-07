4df0dc4def71698ac46919bbc5e11ea0749689f8
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-02-07 16:47:24 +0300

    max choice found, only logic and display

--- webstatistics/appstat/views.py
+++ webstatistics/appstat/views.py
@@ -28,10 +28,18 @@ def results(request, poll_id):
     poll_questions = Question.objects.filter(poll_id=poll_id)
     polling_name = Poll.objects.get(pk=poll_id)
     try: 
+        selected_choice_list = []
+        max_questions = []
         for question in poll_questions:
+               
             selected_choice = question.choices.get(pk=request.POST[question.question_text]) 
             selected_choice.votes +=1
             selected_choice.save()
+            selected_choice_list.append(selected_choice.votes)
+            max_choice = max(selected_choice_list)
+            print(max_choice)
+            selected_choice_list = []
+            max_questions.append(max_choice)      
     except(Choice.DoesNotExist, KeyError):
         context = {
             'poll_questions': poll_questions,
@@ -42,6 +50,7 @@ def results(request, poll_id):
     else:       
         context = {
             'poll_questions' : poll_questions,
-            'polling_name': polling_name,    
+            'polling_name': polling_name,
+            'max_questions': max_questions,    
         }
         return render(request, 'appstat/results.html', context)
